7c45ebc1ad8b3239c168543fb68cb61fda0e6569
Ozzy Callooh <ozzycallooh@gmail.com>
2019-06-28 00:01:44 -0400

    Fix issue where users without usernames break @logged_command decorator

--- src/cmdlogging.py
+++ src/cmdlogging.py
@@ -7,7 +7,10 @@ def logged_command(func):
 		msg = '[{userid}{username}{chatid}]: ' + update.message.text
 		logging.info(msg.format(
 			userid=update.effective_user.id,
-			username=('(@' + update.effective_user.username + ')') if len(update.effective_user.username) > 0 else '',
+			username=('(@' + update.effective_user.username + ')') \
+				if update.effective_user.username != None \
+				and len(update.effective_user.username) > 0 \
+				else '',
 			chatid=',' + str(update.effective_chat.id)
 		))
 		return func(bot, update, *args, **kwargs)
